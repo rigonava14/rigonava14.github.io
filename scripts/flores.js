f4341571c0c7e5681a652a238ddebc27426d7a67
rigonava14 <pajaronava14@gmail.com>
2024-05-10 09:20:36 -0600

    Flores2

---
+++ scripts/flores.js
@@ -0,0 +1,10 @@
+onload = () => {
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+    }, 1000);
+};
+document.getElementById("boton").addEventListener("click", function () {
+    // Redireccionar a la siguiente página
+    window.location.href = "index.html";
+});
